ae7c7d918040e174e201387c1713ee83497a40db
chosson <chosson@users.noreply.github.com>
2022-05-29 21:25:56 -0400

    Changed examples.

--- Exercises/main.cpp
+++ Exercises/main.cpp
@@ -27,43 +27,22 @@ using namespace iter;
 
 
 void runSimpleVtableExample() {
-	class Spam {
-	public:
-		virtual void fn1() const {
-			cout << "Hello, world!" << "\n";
-		}
-
-		virtual int fn2() const {
-			return m1_;
-		}
-
-		int m1_ = 0;
+	struct Spam {
+		virtual void fn1() const { cout << "Spam::fn1()\n"; }
+		virtual int fn2() const { return m1_; }
+		int m1_ = 0x11111111;
 	};
 
-	class Eggs : public Spam {
-	public:
-		void fn1() const override {
-			cout << "Howdy there!" << "\n";
-		}
-
-		int fn2() const override {
-			return m1_ + m2_;
-		}
-
-		int m2_ = 0;
+	struct Eggs : public Spam {
+		void fn1() const override { cout << "Eggs::fn1()\n"; }
+		int fn2() const override { return m1_ + m2_; }
+		int m2_ = 0x22222222;
 	};
 
-	class Parrot : public Eggs {
-	public:
-		void fn1() const override {
-			cout << "Well hey there little fella" << "\n";
-		}
-
-		int fn2() const override {
-			return m1_ + m2_ + m3_;
-		}
-
-		int m3_ = 0;
+	struct Parrot : public Eggs {
+		void fn1() const override { cout << "Parrot::fn1()\n"; }
+		int fn2() const override { return m1_ + m2_ + m3_; }
+		int m3_ = 0x33333333;
 	};
 
 
@@ -81,28 +60,32 @@ void runSimpleVtableExample() {
 
 void runVirtualInheritanceVtableExample() {
 	struct Spam {
-		int m1_ = 0;
+		int m1_ = 0x11111111;
 	};
 
 	struct Eggs : virtual public Spam {
-		int m2_ = 0;
+		int m2_ = 0x22222222;
 	};
 
 	struct Parrot : virtual public Spam {
-		int m3_ = 0;
+		int m3_ = 0x33333333;
 	};
 
 	struct ExParrot : public Eggs, public Parrot {
-		int m4_ = 0;
+		int m4_ = 0x44444444;
 	};
 
 
-	ExParrot foo;
-	foo.m1_ = 0x42;
-	foo.m2_ = 0x69;
-	foo.m3_ = 0x1337;
-	foo.m4_ = 0xDEAD;
-	cout << sizeof(foo) << "\n";
+	Spam spam;
+	Eggs eggs;
+	Eggs eggs2;
+	Parrot parrot;
+	ExParrot exParrot;
+	exParrot.m1_ = 0x55555555;
+	exParrot.m2_ = 0x66666666;
+	exParrot.m3_ = 0x77777777;
+	exParrot.m4_ = 0x88888888;
+	cout << sizeof(exParrot) << "\n";
 }
 
 void runSchoolExample() {
@@ -116,7 +99,7 @@ void runSchoolExample() {
 	// 'ptr' est un pointeur de Etudiant (up-casté de MauvaisEtudiantBanditPasCorrect)
 	// Si on le dynamic_cast en GradStudent*, ça marche puisque MauvaisEtudiantBanditPasCorrect est dérivé
 	// de GradStudent, et donc en est un.
-	if ( dynamic_cast<GraduateStudent*>(ptr) != nullptr )
+	if (dynamic_cast<GraduateStudent*>(ptr) != nullptr)
 		cout << "Sup" << endl;
 	else
 		cout << "Pas sup" << endl;
@@ -195,7 +178,7 @@ void runAmbiguousCallExample() {
 
 int main() {
 	//runAmbiguousCallExample(); cout << "\n\n\n\n";
-	//runSimpleVtableExample(); cout << "\n\n\n\n";
+	runSimpleVtableExample(); cout << "\n\n\n\n";
 	runVirtualInheritanceVtableExample(); cout << "\n\n\n\n";
 	//runSchoolExample(); cout << "\n\n\n\n";
 	//runAnimalExample(); cout << "\n\n\n\n";
